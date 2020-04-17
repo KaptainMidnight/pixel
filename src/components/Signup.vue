ba5c2bc2c8e8fa1896064a5851be61421e43fd49
kaptainmidnight <alexartemkin173@mail.ru>
2020-04-17 18:06:40 +0400

    Bug fix (wrong link in signup)

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -81,7 +81,7 @@
                 }
 
                 if (this.firstName && this.lastName) {
-                    axios.post('https://api-pixelnetwork.truemachine.ru/api/signup', {
+                    axios.post('https://api-pixelnetwork.truemachine.ru/api/auth/signup', {
                         name: this.firstName,
                         surname: this.lastName,
                         email: this.email,
