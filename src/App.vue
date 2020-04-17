7f7fed46515792f64ab3cb3b2d1a8bf2f82e8367
kaptainmidnight <alexartemkin173@mail.ru>
2020-04-17 17:05:58 +0400

    Changed transition color

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,12 @@
         </vue-page-transition>
     </div>
 </template>
+
+<style>
+    .overlay-top[data-v-4c22b934],
+    .overlay-bottom[data-v-4c22b934],
+    .overlay-left[data-v-4c22b934],
+    .overlay-right[data-v-4c22b934] {
+        background: #61B2AA !important;
+    }
+</style>
